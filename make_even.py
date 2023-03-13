488758253334955da175960c6a8ed47700e71342
Pascal Schärli <github@pascscha.ch>
2023-03-13 10:37:10 +0100

    Formatting, pep8 compliance

--- make_even.py
+++ make_even.py
@@ -7,55 +7,60 @@ duplex = True
 
 # remove old output
 try:
-    os.remove(os.path.join(os.getcwd(),folder,"output.pdf"))
-except:
+    os.remove(os.path.join(os.getcwd(), folder, "output.pdf"))
+except BaseException:
     pass
 
-def multiply_pdf(file:str):
+
+def multiply_pdf(file: str):
     total_list = []
     number = int(file.split("_")[0].split("x")[0])
     for i in range(number):
         total_list.append(file)
     return total_list
 
-def make_even(file:str):
+
+def make_even(file: str):
     rewrite_needed = False
-    with open(os.path.join(os.getcwd(),folder,file), 'rb') as pdfFileObj: 
-        pdf=PdfFileReader(pdfFileObj)
-        numPages=pdf.getNumPages()
+    with open(os.path.join(os.getcwd(), folder, file), "rb") as pdfFileObj:
+        pdf = PdfFileReader(pdfFileObj)
+        numPages = pdf.getNumPages()
 
         if numPages % 2 == 1:
             rewrite_needed = True
-            outpdf=PdfFileWriter()
+            outpdf = PdfFileWriter()
             outpdf.appendPagesFromReader(pdf)
             outpdf.addBlankPage()
 
-            with open(os.path.join(os.getcwd(),folder, "temp.pdf"), 'wb') as pdfTempObj:
+            with open(
+                os.path.join(os.getcwd(), folder, "temp.pdf"), "wb"
+            ) as pdfTempObj:
                 outpdf.write(pdfTempObj)
-    
+
     if rewrite_needed:
-        with open(os.path.join(os.getcwd(),folder,"temp.pdf"), 'rb') as pdfTempObj:
-            pdf=PdfFileReader(pdfTempObj)
-            with open(os.path.join(os.getcwd(),folder, file), 'wb') as pdfFileObj:
-                outpdf=PdfFileWriter()
+        with open(os.path.join(os.getcwd(), folder, "temp.pdf"), "rb") as pdfTempObj:
+            pdf = PdfFileReader(pdfTempObj)
+            with open(os.path.join(os.getcwd(), folder, file), "wb") as pdfFileObj:
+                outpdf = PdfFileWriter()
                 outpdf.appendPagesFromReader(pdf)
                 outpdf.write(pdfFileObj)
                 print("made even: " + file)
+
 
 # load all file names
 all_files = []
 
 for filename in os.listdir(os.path.join(os.getcwd(), folder)):
-        if filename.endswith(".pdf"): 
-            all_files.append(filename)
+    if filename.endswith(".pdf"):
+        all_files.append(filename)
 
 # make all files even if desired
 if duplex:
-    for file in all_files: 
+    for file in all_files:
         make_even(file)
     try:
-        os.remove(os.path.join(os.getcwd(),folder,"temp.pdf"))
-    except:
+        os.remove(os.path.join(os.getcwd(), folder, "temp.pdf"))
+    except BaseException:
         pass
 
 # join full pdf
@@ -68,17 +73,17 @@ for file in all_files:
 print(fullPDF)
 
 # merge whole list
-outpdf=PdfFileWriter()
+outpdf = PdfFileWriter()
 openFileStreams = []
 
 for file in fullPDF:
-    pdfFileObj = open(os.path.join(os.getcwd(),folder,file), 'rb')
-    pdf=PdfFileReader(pdfFileObj)
+    pdfFileObj = open(os.path.join(os.getcwd(), folder, file), "rb")
+    pdf = PdfFileReader(pdfFileObj)
     outpdf.appendPagesFromReader(pdf)
     openFileStreams.append(pdfFileObj)
 
-with open(os.path.join(os.getcwd(),folder, "output.pdf"), 'wb') as pdfTempObj:
-                outpdf.write(pdfTempObj)
+with open(os.path.join(os.getcwd(), folder, "output.pdf"), "wb") as pdfTempObj:
+    outpdf.write(pdfTempObj)
 
 for stream in openFileStreams:
     stream.close()
